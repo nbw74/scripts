11f965cf71594f87e5a8f3ed1985d67d3d18ee81
Nbw <nbw.adm@gmail.com>
2018-02-26 02:04:06 +0400

    misc/tls-a.sh first release

--- misc/tls-a.sh
+++ misc/tls-a.sh
@@ -14,9 +14,10 @@ readonly PATH=/bin:/usr/bin:/sbin:/usr/sbin
 readonly bn="$(basename $0)"
 readonly LOGERR=$(mktemp --tmpdir ${bn%\.*}.XXXX)
 readonly BIN_REQUIRED="aunpack"
+readonly ping_opts="-i0.2 -W1 -c5 -q"
 # CONSTANTS END
 
-typeset OPTTAIL="" IPADDR=""
+typeset OPTTAIL="" ADDRESS=""
 
 main() {
     local fn=$FUNCNAME
@@ -32,20 +33,38 @@ main() {
         fi
     done
 
-    aunpack $OPTARG
-    cd ${OPTARG%\.*}
+    aunpack $OPTTAIL
+    mv ${OPTTAIL%\.*}.key ${OPTTAIL%\.*}/
+    cd ${OPTTAIL%\.*}
 
     local cert=$(ls -1 *.crt|head -1)
     local cert_final=${cert//_/.}
     local domain=${cert_final%\.*}
 
     cat $cert ${cert%\.*}.ca-bundle > $cert_final
-    mv ../${OPTARG%\.*}.key ${domain}.key
+    mv ${OPTTAIL%\.*}.key ${domain}.key
 
-    if [[ -n "$IPADDR" ]]; then
-        scp $cert_final ${domain}.key ${IPADDR}:
+    if [[ -z "$ADDRESS" ]]; then
+        echo_info "remote server isn't specified, using '$domain'"
+
+        ADDRESS=$(dig +short $domain|head -1)
+
+        if [[ -z "$ADDRESS" ]]; then
+            echo_err "cannot resolve '$domain'"
+            false
+        fi
     fi
 
+    if ! ping $ping_opts $ADDRESS >/dev/null
+    then
+        echo_err "'$ADDRESS' is unreachable"
+        false
+    fi
+
+    echo_info "try scp files to '$ADDRESS'..."
+    scp $cert_final ${domain}.key ${ADDRESS}:
+
+    echo_ok
     exit 0
 }
 
@@ -54,7 +73,7 @@ except() {
     local no=${1:-no_line}
 
     if [[ -t 1 ]]; then
-        echo "* FATAL: error occured in function '$fn' on line ${no}. Output: '$(cat ${LOGERR}|awk '$1=$1' ORS=' ')'" 1>&2
+        echo_fatal "error occured in function '$fn' on line ${no}." 1>&2
     fi
 
     logger -p user.err -t "$bn" "* FATAL: error occured in function '$fn' on line ${no}."
@@ -80,7 +99,7 @@ usage() {
 while getopts "a:h" OPTION; do
     case $OPTION in
         a)
-            IPADDR=$OPTARG
+            ADDRESS=$OPTARG
             ;;
         h)
             usage; exit 0
@@ -95,6 +114,20 @@ if [[ "${1:-NOP}" == "NOP" ]]; then
     usage
     exit 1
 fi
+
+readonly C_RST="tput sgr0"
+readonly C_RED="tput setaf 1"
+readonly C_GREEN="tput setaf 2"
+readonly C_YELLOW="tput setaf 3"
+readonly C_BLUE="tput setaf 4"
+readonly C_CYAN="tput setaf 6"
+readonly C_WHITE="tput setaf 7"
+
+echo_err() { $C_WHITE; echo "* ERROR: $*" 1>&2; $C_RST; }
+echo_fatal() { $C_RED; echo "* FATAL: $*" 1>&2; $C_RST; }
+echo_warn() { $C_YELLOW; echo "* WARNING: $*" 1>&2; $C_RST; }
+echo_info() { $C_BLUE; echo "* INFO: $*" 1>&2; $C_RST; }
+echo_ok() { $C_GREEN; echo "* OK" 1>&2; $C_RST; }
 
 main
 
